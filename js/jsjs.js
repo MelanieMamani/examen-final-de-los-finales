fa262c3303d70300fce67a225a7fb4905f837770
Melanie <melaniekarolmamani@gmail.com>
2023-12-12 03:56:28 -0500

    first commit

---
+++ js/jsjs.js
@@ -0,0 +1,18 @@
+var audio = document.getElementById('audio');
+var playPauseBtn = document.getElementById('playPauseBtn');
+var progressBar = document.getElementById('progressBar');
+var currentTimeElement = document.getElementById('currentTime');
+var totalTimeElement = document.getElementById('totalTime');
+var albumCover = document.getElementById('albumCover');
+
+function togglePlayPause() {
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.textContent = '🐋';
+        albumCover.style.animation = 'rotation 5s infinite linear';
+    } else {
+        audio.pause();
+        playPauseBtn.textContent = '🐳';
+        albumCover.style.animation = 'none';
+    }
+}
